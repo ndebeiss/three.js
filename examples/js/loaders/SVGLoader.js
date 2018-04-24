ec8a1041f290fe0f72cdb5584410f2c74136b9e3
Nicolas Debeissat <ndebeiss@gmail.com>
2018-04-24 16:09:51 +0200

    x_axis_rotation is in degree, adding step 3, and correction for relative coords

--- examples/js/loaders/SVGLoader.js
+++ examples/js/loaders/SVGLoader.js
@@ -224,9 +224,13 @@ THREE.SVGLoader.prototype = {
 						var numbers = parseFloats( data );
 						for ( var j = 0, jl = numbers.length; j < jl; j += 7 ) {
 
+							point.x = numbers[ j + 5 ];
+							point.y = numbers[ j + 6 ];
 							var radius = { x: numbers[ j ], y: numbers[ j + 1 ] };
-							var end = { x: numbers[ j + 5 ], y: numbers[ j + 6 ] };
-							svgEllipsisToThreeEllipsis( path, control, radius, numbers[ j + 2 ], numbers[ j + 3 ], numbers[ j + 4 ], end );
+							var x_axis_rotation = numbers[ j + 2 ] * Math.PI / 180;
+							svgEllipsisToThreeEllipsis( path, control, radius, x_axis_rotation, numbers[ j + 3 ], numbers[ j + 4 ], point );
+							control.x = point.x;
+							control.y = point.y;
 
 						}
 						break;
@@ -261,7 +265,7 @@ THREE.SVGLoader.prototype = {
 							control.x = point.x;
 							control.y = point.y;
 							path.lineTo( point.x, point.y );
-						} 
+						}
 						break;
 
 					case 'l':
@@ -381,21 +385,34 @@ THREE.SVGLoader.prototype = {
 		 * aX, aY, xRadius, yRadius, aStartAngle, aEndAngle, aClockwise, aRotation
 		 */
 		function svgEllipsisToThreeEllipsis( path, start, radius, x_axis_rotation, large_arc_flag, sweep_flag, end ) {
-			
+
 			//F.6.6 Correction of out-of-range radii
 			//Step 2: Ensure radii are positive
 			var rX = Math.abs( radius.x );
 			var rY = Math.abs( radius.y );
-			//Step 3: Ensure radii are large enough looks like over engineering
+
 			// Step 1: Compute (x1′, y1′)
 			var midDist = new THREE.Vector2().subVectors( start, end ).multiplyScalar( 0.5 );
 			var x1p = Math.cos( x_axis_rotation ) * midDist.x + Math.sin( x_axis_rotation ) * midDist.y;
 			var y1p = - Math.sin( x_axis_rotation ) * midDist.x + Math.cos( x_axis_rotation ) * midDist.y;
+
 			// Step 2: Compute (cx′, cy′)
 			var rxs = rX * rX;
 			var rys = rY * rY;
 			var x1ps = x1p * x1p;
 			var y1ps = y1p * y1p;
+
+			//Step 3: Ensure radii are large enough
+			var cr = x1ps / rxs + y1ps / rys;
+			if (cr > 1) {
+				//scale up rX,rY equally so cr == 1
+				var s = Math.sqrt(cr);
+				rX = s * rX;
+				rY = s * rY;
+				rxs = rX * rX;
+				rys = rY * rY;
+			}
+			
 			var dq = ( rxs * y1ps + rys * x1ps );
 			var pq = ( rxs * rys - dq ) / dq;
 			var q = Math.sqrt( pq );
